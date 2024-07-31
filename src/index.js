48b32caa9f9e9e544bff9a9d938b05d0544fd42a
Thomas <todeschenes@outlook.com>
2024-07-31 16:38:44 -0400

    BrowserRouter

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='https://thomd7.github.io/legendaires'>
+    <BrowserRouter basename='https://thomd7.github.io/legendaires/'>
       <Helmet>
         <title>{clanName}</title>
         <link rel="icon" href="/favicon.ico" />
