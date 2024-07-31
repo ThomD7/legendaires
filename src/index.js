c8d929fc1f7498328f57c74442d2bd07a6cbdbab
Thomas <todeschenes@outlook.com>
2024-07-31 16:05:20 -0400

    Fix URL

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import { BrowserRouter } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import { clanName } from './config';
 
+const routerBaseName = process.env.PUBLIC_URL;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={routerBaseName}>
       <Helmet>
         <title>{clanName}</title>
         <link rel="icon" href="/favicon.ico" />
