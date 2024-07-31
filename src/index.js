830ba84bd428c3a5b039b8a2784e8c19a875c00e
Thomas <todeschenes@outlook.com>
2024-07-31 16:27:36 -0400

    HashRouter

--- src/index.js
+++ src/index.js
@@ -2,23 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import { clanName } from './config';
-
-const routerBaseName = process.env.PUBLIC_URL;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={routerBaseName}>
+    <HashRouter>
       <Helmet>
         <title>{clanName}</title>
         <link rel="icon" href="/favicon.ico" />
       </Helmet>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
